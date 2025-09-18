a82ec05ec72b0d0059dcee7c121382f93e4b03ba
Rafael CELESTRE <rafael.celestre@gmail.com>
2025-09-18 19:11:44 +0200

    fix: removed beamline plots from __init__
     On branch main
     Changes to be committed:
    	modified:   src/barc4beams/__init__.py
    	deleted:    src/barc4beams/utils.py

--- src/barc4beams/__init__.py
+++ src/barc4beams/__init__.py
@@ -9,13 +9,10 @@ from ._version import __version__
 from .adapters import merge_standard_beams, to_standard_beam
 from .beam import Beam
 from .io import read_beam, read_json_stats, save_beam, save_json_stats
-from .parsing import parse_shadow_sys_info
 from .stats import calc_envelope_from_moments, get_focal_distance, get_statistics
 from .viz import (
     plot,
     plot_beam,
-    plot_beamline,
-    plot_beamline_configs,
     plot_divergence,
     plot_energy,
     plot_energy_vs_intensity,
@@ -30,15 +27,12 @@ __all__ = [
     "get_statistics",
     "get_focal_distance",
     "calc_envelope_from_moments",
-    "parse_shadow_sys_info"
     "plot",
     "plot_beam",
     "plot_divergence",
     "plot_energy",
     "plot_energy_vs_intensity",
     "plot_phase_space",
-    "plot_beamline",
-    "plot_beamline_configs",
     "save_beam",
     "read_beam",
     "save_json_stats",
--- src/barc4beams/utils.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-only
-# Copyright (c) 2025 Synchrotron SOLEIL
-
-"""
-utils.py - general utility functions
-"""
-
-from __future__ import annotations
